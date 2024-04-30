d73be8161465ad323bde3109102bd2fb14fbd215
luke-h1 <luke.howsam@yahoo.com>
2024-04-30 14:24:51 +0100

    initial commit

---
+++ src/pages/api/counter.ts
@@ -0,0 +1,30 @@
+import redis from "@/util/redis";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+
+  switch(req.method) {
+    case 'GET':
+      const response = await redis.get('counter') || '0';
+      console.info('[GET counter]: Retreived count', response);
+      return res.status(200).json({ count: response });
+
+    case 'POST':
+      const count = await redis.incr('counter');
+      console.info('[POST counter]: Incremented count', count);
+      return res.status(200).json({ count });
+
+    case 'DELETE':
+      const delCount = await redis.del('counter');
+      console.info('[DELETE counter]: Deleted count', delCount);
+      return res.status(200).json({ count: 0 });
+
+    default:
+      return res.status(405).end();
+
+  }
+}
